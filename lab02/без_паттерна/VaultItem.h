08486f16d666aec6c350c2e2a83984b7cd089e06
marienbaum77 <145770110+marienbaum77@users.noreply.github.com>
2026-04-04 00:30:03 +0700

    Update VaultItem.h

--- lab02/без_паттерна/VaultItem.h
+++ lab02/без_паттерна/VaultItem.h
@@ -5,6 +5,7 @@
 #include <QString>
 #include <QVector>
 #include <QVariantMap>
+#include <QRegularExpression>
 #include <QJsonObject>
 #include <QJsonArray>
 
@@ -16,22 +17,33 @@ class VaultItem : public QObject {
 public:
     explicit VaultItem(QString t, QObject *parent = nullptr) : QObject(parent), m_title(t) {}
     QString m_title;
-    double m_graphX = 0;
-    double m_graphY = 0;
+    double m_graphX;
+    double m_graphY;
 signals:
     void titleChanged();
     void graphPosChanged();
+    void statsChanged();
 };
 
 class Note : public VaultItem {
     Q_OBJECT
-    Q_PROPERTY(QString content MEMBER m_content NOTIFY contentChanged)
+    Q_PROPERTY(QString content READ content WRITE setContent NOTIFY contentChanged)
 public:
     explicit Note(QString t, QObject *parent = nullptr) : VaultItem(t, parent) {}
     QString m_content;
-    
-    QVariantMap getOwnStats() const {
-        return { {"notes", 1}, {"chars", m_content.length()} };
+    QString content() const { return m_content; }
+    void setContent(const QString &c) { m_content = c; emit contentChanged(); emit statsChanged(); }
+
+    QVariantMap getNoteStats() const {
+        return { {"notes", 1}, {"chars", m_content.length()}, 
+                 {"words", m_content.split(QRegularExpression("\\s+"), Qt::SkipEmptyParts).count()} };
+    }
+
+    QJsonObject noteToJson() const {
+        QJsonObject obj;
+        obj["type"] = "note"; obj["title"] = m_title; obj["content"] = m_content;
+        obj["x"] = m_graphX; obj["y"] = m_graphY;
+        return obj;
     }
 signals:
     void contentChanged();
@@ -39,28 +51,47 @@ signals:
 
 class Folder : public VaultItem {
     Q_OBJECT
+    Q_PROPERTY(QVariantList subFoldersQml READ subFoldersQml NOTIFY childrenChanged)
+    Q_PROPERTY(QVariantList notesQml READ notesQml NOTIFY childrenChanged)
 public:
     explicit Folder(QString t, QObject *parent = nullptr) : VaultItem(t, parent) {}
-
-    QVector<Folder*> subFolders;
-    QVector<Note*> notes;
-
-    Q_PROPERTY(QVariantList subFoldersQml READ subFoldersQml NOTIFY changed)
-    Q_PROPERTY(QVariantList notesQml READ notesQml NOTIFY changed)
+    
+    QVector<Folder*> m_subFolders;
+    QVector<Note*> m_notes;
 
     QVariantList subFoldersQml() const {
-        QVariantList list;
-        for (auto f : subFolders) list << QVariant::fromValue(f);
-        return list;
+        QVariantList l; for (auto f : m_subFolders) l << QVariant::fromValue(f);
+        return l;
     }
     QVariantList notesQml() const {
-        QVariantList list;
-        for (auto n : notes) list << QVariant::fromValue(n);
-        return list;
+        QVariantList l; for (auto n : m_notes) l << QVariant::fromValue(n);
+        return l;
     }
 
-signals:
-    void changed();
-};
+    QVariantMap getFolderStats() const {
+        int n = 0, w = 0, c = 0;
+        for (auto note : m_notes) {
+            auto s = note->getNoteStats();
+            n += s["notes"].toInt(); w += s["words"].toInt(); c += s["chars"].toInt();
+        }
+        for (auto sub : m_subFolders) {
+            auto s = sub->getFolderStats();
+            n += s["notes"].toInt(); w += s["words"].toInt(); c += s["chars"].toInt();
+        }
+        return { {"notes", n}, {"words", w}, {"chars", c} };
+    }
 
+    QJsonObject folderToJson() const {
+        QJsonObject obj;
+        obj["type"] = "folder"; obj["title"] = m_title;
+        obj["x"] = m_graphX; obj["y"] = m_graphY;
+        QJsonArray ch;
+        for (auto f : m_subFolders) ch.append(f->folderToJson());
+        for (auto n : m_notes) ch.append(n->noteToJson());
+        obj["children"] = ch;
+        return obj;
+    }
+signals:
+    void childrenChanged();
+};
 #endif
